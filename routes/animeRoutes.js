8fc9670caecab2ae1f24b8c538541b0a9cfe776c
PolixTer <clevertonruppenthal1@gmail.com>
2024-12-18 15:03:43 -0300

    Ajutes controller

--- routes/animeRoutes.js
+++ routes/animeRoutes.js
@@ -1,4 +1,4 @@
-const { getAnime } = require("../controllers/animeController");
+const { getAnime } = require("../controllers/AnimeController");
 
 async function animeRoutes(fastify, options) {
   fastify.get("/anime/:id", getAnime);
