50cf55cfec32035a78fb68a1c9b6cccd0b3acb70
PolixTer <clevertonruppenthal1@gmail.com>
2025-03-14 11:41:23 -0300

    Ajuste identação

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Aguardando o PostgreSQL ficar disponível..."
 # Aguarda até que o PostgreSQL esteja aceitando conexões na porta 5432
 while ! nc -z $DB_HOST 5432; do
-  sleep 1
+    sleep 1
 done
 echo "PostgreSQL disponível!"
 
