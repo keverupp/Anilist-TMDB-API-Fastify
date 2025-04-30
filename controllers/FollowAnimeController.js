1edf1b194b2597e8955b3c894b7872ed12b64a8f
PolixTer <clevertonruppenthal1@gmail.com>
2025-04-30 13:21:42 +0100

    Ajuste resposta

--- controllers/FollowAnimeController.js
+++ controllers/FollowAnimeController.js
@@ -2,7 +2,7 @@ const knex = require("knex")(require("../knexfile").development);
 
 async function toggleFollowAnime(req, reply) {
   const { anime_id } = req.body;
-  const user_id = req.user.id; // Obtém o ID do usuário autenticado
+  const user_id = req.user.id;
 
   if (!anime_id) {
     return reply.status(400).send({
@@ -12,42 +12,37 @@ async function toggleFollowAnime(req, reply) {
   }
 
   try {
-    // Verificar se o anime existe
     const animeExists = await knex("animes").where({ id: anime_id }).first();
     if (!animeExists) {
       return reply.status(404).send({
         error: "Not Found",
-        message: "Ops. O anime não foi encontrado.",
+        message: "Anime não encontrado.",
       });
     }
 
-    // Verificar se o anime já está sendo seguido
     const existingFollow = await knex("anime_follows")
       .where({ anime_id, user_id })
       .first();
 
     if (existingFollow) {
-      // Remover o "seguir"
       await knex("anime_follows").where({ anime_id, user_id }).del();
-
       return reply.status(200).send({
-        message: "Você desfavoritou o anime.",
+        message: "Anime removido dos favoritos.",
       });
     }
 
-    // Registrar o "seguir"
     await knex("anime_follows").insert({
       anime_id,
       user_id,
     });
 
     return reply.status(201).send({
-      message: "Anime favoritado.",
+      message: "Anime adicionado aos favoritos.",
     });
   } catch (error) {
-    console.error("Erro ao favoritar/desfavoritar anime:", error);
+    console.error("Erro ao atualizar favoritos:", error);
     return reply.status(500).send({
-      error: "Erro ao alternar o estado de favoritar anime.",
+      error: "Erro ao atualizar anime nos favoritos.",
     });
   }
 }
@@ -56,18 +51,17 @@ async function listFollowedAnimes(req, reply) {
   const user_id = req.user.id;
 
   try {
-    // Consultar os animes seguidos pelo usuário
     const followedAnimes = await knex("anime_follows")
       .join("animes", "anime_follows.anime_id", "animes.id")
       .where("anime_follows.user_id", user_id)
       .select("animes.id", "animes.name", "animes.poster_path");
 
     return reply.status(200).send({
-      message: "Lista de animes favoritados.",
+      message: "Animes favoritados listados com sucesso.",
       animes: followedAnimes,
     });
   } catch (error) {
-    console.error("Erro ao listar animes favoritados:", error);
+    console.error("Erro ao listar favoritos:", error);
     return reply.status(500).send({
       error: "Erro ao listar animes favoritados.",
     });
